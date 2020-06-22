87b26a23a4994fd368d721af52f1a509579d51a7
smrinalsingh <s.mrinalsingh@gmail.com>
2020-06-22 23:42:32 +0530

    Add project files.

---
+++ NetCommander/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NetCommander
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Process process = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.SetOut(new StreamWriter("log.log"));
+            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
+        }
+
+        private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            e.Cancel = true;
+            Process.Start("shutdown", "-a");
+            Process.Start("shutdown", "/r /t 0");
+        }
+
+        private string RunScript(string scriptText)
+        {
+            // create Powershell runspace
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+
+            // open it
+            runspace.Open();
+
+            // create a pipeline and feed it the script text
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript(scriptText);
+            pipeline.Commands.Add("Out-String");
+
+            // execute the script
+            Collection<PSObject> results = pipeline.Invoke();
+
+            // close the runspace
+            runspace.Close();
+
+            // convert the script result into a single string
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private void lstScripts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string scriptPath = lstScripts.SelectedItem.ToString();
+            if (File.Exists(scriptPath))
+                GetProperties(scriptPath);
+            else
+                Console.WriteLine(scriptPath + "Not Found");
+        }
+
+        private void GetProperties(string scriptPath)
+        {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.Add($"Get-Command {scriptPath}");
+
+            // execute the script
+            Collection<PSObject> results = pipeline.Invoke();
+
+            // close the runspace
+            runspace.Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            SystemEvents.SessionEnding -= SystemEvents_SessionEnding;
+            base.OnClosed(e);
+        }
+
+        private void btnAddScript_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog()
+            {
+                Filter = "PowerShell file (*.ps1)|*.ps1",
+            };
+            openFile.ShowDialog();
+            string filePath = openFile.FileName;
+            var info = File.GetLastWriteTime(filePath).ToString("dd-MM-yyyy HH:mm:ss");
+            if (!string.IsNullOrEmpty(filePath) && File.Exists(filePath))
+                lstScripts.Items.Add(new ScriptsDetails() { ScriptPath = filePath, ModDate = info });
+        }
+    }
+
+    public class ScriptsDetails
+    {
+        public string ScriptPath { get; set; }
+        public string ModDate { get; set; }
+    }
+}
---
+++ NetCommander/Scripts/RemoteCommander.ps1
@@ -0,0 +1,120 @@
+﻿param ([Parameter(Mandatory)][string]$file, [Parameter(Mandatory)][string]$password, [Parameter(Mandatory)][string]$command, 
+        [Parameter()][string]$StartAt = '0', [switch]$usesys, [switch]$showerror)
+
+function Invoke-EscapeString([string]$str){
+    $str = $str -replace "`"", "`"`""
+    return $str
+}
+
+function Invoke-OnHost(){
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string]$targethost,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('cmd', 'powershell')]
+        [string]$app,
+    
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$cmd,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$pswd,
+
+        [Parameter()]
+        [string]$userID = 'Administrator',
+
+        [Parameter()]
+        [switch]$displayError,
+
+        [Parameter()]
+        [switch]$useSystem
+    )
+    
+    Write-Host "-- Output: $targetHost --" -ForegroundColor Yellow
+
+    ## Collect the output and error output
+    $errorOutput = [IO.Path]::GetTempFileName()
+    $cmd = Invoke-EscapeString -str $cmd
+
+    if ($useSystem){
+        $output = psexec /acceptEula "\\$targethost" -s -u $userID -p $pswd $app /c $cmd 2>$errorOutput
+    }
+    elseif (-not $useSystem){
+        $output = psexec /acceptEula "\\$targethost" -h -u $userID -p $pswd $app /c $cmd 2>$errorOutput
+    }
+    
+
+    ## Check for any errors
+    $errorContent = Get-Content $errorOutput
+    Remove-Item $errorOutput
+
+    ## Output and Error
+    
+    foreach ($line in $output){
+        Write-Host $line -ForegroundColor Cyan
+    }
+
+    if ($displayError){
+        foreach ($line in $errorContent){
+            Write-Host $line -ForegroundColor Red
+        }
+    }
+
+    Write-Host "`n-- End Output: $targetHost --" -ForegroundColor Yellow
+}
+
+function Invoke-CommandOnHosts(){
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string]$hosts,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('cmd', 'powershell')]
+        [string]$app,
+    
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$cmd,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$pswd,
+
+        [Parameter()]
+        [string]$UserID = 'Administrator',
+
+        [switch]$DisplayError,
+
+        [Parameter()]
+        [switch]$UseSystem,
+
+        [Parameter()]
+        [string]$WaitUntil = '0'
+    )
+
+    if ($WaitUntil -ne '0'){
+        Wait-Until -Time $WaitUntil
+    }
+
+    foreach ($hos in (Get-Content $hosts)){
+        Invoke-OnHost -targethost $hos -cmd $cmd -pswd $pswd -app $app -displayError:$DisplayError -useSystem:$UseSystem
+    }
+}
+
+function Wait-Until([string]$Time){
+    try{
+        (New-TimeSpan -End $Time).TotalSeconds | Sleep
+    } catch {
+        
+    }
+
+}
+
+Invoke-CommandOnHosts -hosts $file -app cmd -cmd $command -pswd $password -UseSystem:$usesys -DisplayError:$showerror -WaitUntil $StartAt
